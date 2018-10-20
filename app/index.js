35c1bb53ff53234a82dbf933f945c19d196a3bea
Chris Underwood <cunderwoodmn@gmail.com>
2018-10-19 22:52:56 -0500

    added webserver dev

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-require('index.css'); //this format works with style-loader in Babel
+require('./index.css'); //this format works with style-loader in Babel
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return <div>Hello Fuckface</div>;
   }
 }
 
