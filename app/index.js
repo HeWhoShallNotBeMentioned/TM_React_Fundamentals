7b0d78f809e58824fae16916d082913e2db26783
Chris Underwood <cunderwoodmn@gmail.com>
2018-10-19 20:26:51 -0500

    fixed errors with webpac

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-require('./index.css');
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('index.css'); //this format works with style-loader in Babel
+
 class App extends React.Component {
   render() {
     return <div>Hello World!</div>;
   }
 }
+
 ReactDOM.render(<App />, document.getElementById('app'));
