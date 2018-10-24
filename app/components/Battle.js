3d5e4ac1e2b047d62b3c51775960acdb97299b99
Chris Underwood <cunderwoodmn@gmail.com>
2018-10-23 21:36:10 -0500

    added code to show the starting forms for players in battle

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,8 +1,49 @@
 const React = require('react');
+const PlayerInput = require('./PlayerInput');
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username) {
+    this.setState(function() {
+      let newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] =
+        'https://github.com/' + username + '.png?size=200';
+      return newState;
+    });
+  }
   render() {
-    return <div>Battle!</div>;
+    let playerOneName = this.state.playerOneName;
+    let playerTwoName = this.state.playerTwoName;
+
+    return (
+      <div className="row">
+        {!playerOneName && (
+          <PlayerInput
+            id="playerOne"
+            label="Player One"
+            onSubmit={this.handleSubmit}
+          />
+        )}
+        {!playerTwoName && (
+          <PlayerInput
+            id="playerTwo"
+            label="Player Two"
+            onSubmit={this.handleSubmit}
+          />
+        )}
+      </div>
+    );
   }
 }
 
